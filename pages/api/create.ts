e6764a87035b37b67c3e74f8084165a99790a7d5
nadz <mc4nadia@gmail.com>
2022-11-27 21:10:53 +0800

    debug API not sending proper response

--- pages/api/create.ts
+++ pages/api/create.ts
@@ -28,45 +28,55 @@ export default async function handler(
         caption: ""
     }
     
-    form.parse(req, async function (err:any, fields:any, files:any) {
-        
-        const caption = fields.caption;
-        const file = files.file;
-        const imgData = fs.readFileSync(file.filepath);
-        
-        /**
-         * Check if directory exist, if not it will create the directory
-         * temporarily put the image in ./public/temp
-         * upload the image in cloudinary
-         * if successfully uploaded, save the image data in the database
-         * then remove the copied image from the temp folder
-         */
-        fs.ensureDirSync(path);
-        let filepathCopy = `${path}/${file.originalFilename}`;
-        fs.writeFileSync(filepathCopy, imgData);
-        
-        await new Promise((resolve, reject) => {
-            cloudinary.v2.uploader.upload(filepathCopy)
-            .then( (result: any) => {
-                uploadData = {
-                    _id: result.public_id,
-                    datePosted: new Date(result.created_at),
-                    url: result.url,
-                    caption: caption
-                };
-                resolve(result);
-            })
-        })
-        .then(async (data) => {
-            fs.unlinkSync(filepathCopy);
-            await client.db().collection("picture_posts").insertOne(uploadData);
+    await new Promise((formResolve, formReject) => {
+        form.parse(req, async function (err:any, fields:any, files:any) {
+            if(!files) {
+                formReject(files);
+                return;
+            }
 
-            res.status(201).json({ status: "success", result: uploadData });
-            console.log("Successfully created!");
-        })
-        .catch((err) => {
-            console.log("Upload failed: "+err);
-            res.status(500).json({ status: "failed" });
-        })
+            const caption = fields.caption;
+            const file = files.file;
+            const imgData = fs.readFileSync(file.filepath);
+            
+            /**
+             * Check if directory exist, if not it will create the directory
+             * temporarily put the image in ./public/temp
+             * upload the image in cloudinary
+             * if successfully uploaded, save the image data in the database
+             * then remove the copied image from the temp folder
+             */
+            fs.ensureDirSync(path);
+            let filepathCopy = `${path}/${file.originalFilename}`;
+            fs.writeFileSync(filepathCopy, imgData);
+            
+            await new Promise((uploadResolve, uploadReject) => {
+                cloudinary.v2.uploader.upload(filepathCopy)
+                .then( (result: any) => {
+                    uploadData = {
+                        _id: result.public_id,
+                        datePosted: new Date(result.created_at),
+                        url: result.url,
+                        caption: caption
+                    };
+                    uploadResolve(result);
+                })
+            })
+            .then(async (data) => {
+                fs.unlinkSync(filepathCopy);
+                await client.db().collection("picture_posts").insertOne(uploadData);
+    
+                res.status(201).json({ status: "success", result: uploadData });
+                console.log("Successfully created!");
+            })
+            .catch((err) => {
+                console.log("Upload failed: "+err);
+                res.status(500).json({ status: "failed" });
+            })
+        }) //endOfFormParse
+    })
+    .catch((err) => {
+        console.log("Upload failed: "+err);
+        res.status(500).json({ status: "failed" });
     });
 }
